49d092b724d8b45a02a5d84b43624609224218b1
Terran Leung <leunte@leunte-macbookpro3.roam.corp.google.com>
2018-11-29 13:08:43 -0500

    Updated rl invest

--- invest/ispy/forrester.js
+++ invest/ispy/forrester.js
@@ -1,43 +1,18 @@
-define('vix_thresholds',[
-  './fetch_single_stock_history',
-  './option_chain_fetcher',
-], async function(
-  fetch_historical,
-  fetch_option_chain,
-) {
-  // create vix thresholds
-  const vix = await fetch_historical('VIX');
-  const thresholds = new Array(20).fill(0).map((n,index)=>index+10).map(threshold => {
-    return vix.map((day,index) => ({
-        index,
-        valid: day.close > threshold - .5 && day.close < threshold + .5
-      }))
-      .filter(index_check => index_check.valid)
-      .map(index_check => index_check.index);
-  });
-  return thresholds;
-});
-
-define('strategy',['utils/pct_change'],(pct_change)=>{
-  return (arr) => {
-
-    return arr.map((day, index) =>{
-      // given each day's return, how much is made with an option
-    });
-  };
-});
+const ACTIONS = {
+  LONG: 1,
+  HOLD: 0,
+  SHORT: -1,
+};
 
 define([
   './fetch_single_stock_history',
   './option_chain_fetcher',
-  'strategy',
   'dt',
   '../sharpe_ratio',
   'utils/mean'
 ], function(
   fetch_historical,
   fetch_option_chain,
-  strategy,
   dt,
   sharpe_ratio,
   mean,
@@ -52,9 +27,10 @@ define([
         // 'XLF',
         // 'XLK',
       ];
-
+      let training_set = [];
       const historical = await Promise.all(securities.map(fetch_historical));
-      const trained_model = train(10); // do something with trained model?
+      const trained_model = train(3); // do something with trained model?
+      
 
       function train(iterations) {
         return new Array(iterations).fill(historical).reduce((model, historical, i) => {
@@ -66,39 +42,56 @@ define([
             .sort((a,b) => b.performance - a.performance);
           const performance = ranked_series.map(agg => agg.performance);
           console.log(i,'performance', mean(performance), sharpe_ratio(performance),performance);
+          
+          training_set = ranked_series.map(agg => agg.series)
+            .slice(0, ranked_series.length / 4)
+            .concat(training_set)
+            .slice(0,20000);
 
           return new dt.RandomForest({
-            trainingSet: ranked_series.map(agg => agg.series).slice(0, ranked_series.length / 5),
+            trainingSet: training_set,
             categoryAttr: 'action',
             ignoredAttributes: []
           }, 128);
 
-        }, {predict: () => Math.random() > 1/2 ? 1 : -1});
+        }, {predict: () => Math.round(Math.random() * 100)});
       }
 
       function run_simulation(historical, model) {
         const max_valid_index = Math.min.apply(Math, historical.map(arr=>arr.length)) - 201;
         // run n number of simulations trading our historical universe
-        return new Array(8000).fill(historical).map(historical => {
+        return new Array(1000).fill(historical).map(historical => {
           // for each series of our historical set, trade
           return historical.map(series => runTest(series));
 
           function runTest(series) {
             // generate sample periods to trade
-            const indexes = new Array(100).fill(0).map(n => Math.floor(Math.random() * max_valid_index)).sort((a,b) => a - b);
-            return indexes.map(index => {
+            const indexes = new Array(50).fill(0).map(n => Math.floor(Math.random() * max_valid_index)).sort((a,b) => a - b);
+            // for each index, calculate actions
+            const trades = indexes.map(index => {
               const features = extract_features(series, index+1);
               const action = policy(features);
               const change = pct_change(series[index+1].close,series[index].close);
               const outcome = action == 1 ? 1+change : 1;
               return {index, action, outcome, features};
             });
+
+            const outcomes = trades.map(trade => {
+
+              const outcome = action == 1 ? 1+change : 1;
+
+              return Object.assign({}, trade, {outcome});
+            });
+
+            return outcomes;
           }
 
           function policy(features) {
-              return Math.random() < .8 ?
+              const action =  Math.random() < .8 ?
                 exploit(features):// exploit
                 explore(features);// explore
+
+              return action;
           }
 
           function extract_features(series, index) {
@@ -110,11 +103,17 @@ define([
           }
 
           function explore(features) { // do nothing with features, random act
-            return Math.random() > 1/2 ? 1 : 0;
+            return Math.random() > 1/2 ? ACTIONS.LONG : ACTIONS.SHORT;
           }
 
           function exploit(features) { // predict based on features
-            return model.predict(features) || 0;
+            const prediction = model.predict(features);
+            const determine_action = () => ACTIONS[prediction];
+            const throw_error = () => {
+              throw new Error('Action undefined')
+            };
+            
+            return prediction ?  determine_action() : throw_error();
           }
         });
       }
